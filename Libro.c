9207dd786bec13a743f9a706b321830ea4f7398f
SidneySJk <125586766+SidneySJk@users.noreply.github.com>
2025-09-07 13:53:12 -0500

    Add files via upload

--- Libro.c
+++ Libro.c
@@ -5,19 +5,19 @@
 
 typedef struct {
     char* nombreLibro;
-    char* caracteres;
+    //char* caracteres;
     float* precioLibro;
     int codigoLibro;
-    int* anioPublicacion;
+    int* cantidad;
 } Libro;
 
 Libro CrearLibro() {
 
     Libro lib;
-    lib.caracteres = (char *)malloc(100 * sizeof(char));
+    //lib.caracteres = (char *)malloc(100 * sizeof(char));
     lib.nombreLibro = NULL;
     lib.precioLibro = (float*)malloc(sizeof(float));
-    lib.anioPublicacion  = (int*)malloc(sizeof(int));
+    lib.cantidad = (int*)malloc(sizeof(int));
     lib.codigoLibro = 0;
     return lib;
 }
@@ -26,42 +26,40 @@ void registrarLibro(Libro *lib) { //struct Libro* libro
 
     printf("Nombre del libro:");
     
-    scanf("%s", lib->caracteres);
-    lib->nombreLibro = espacioDeMemoria(lib->caracteres);
+    //scanf("%s", lib->nombreLibro);
+    lib->nombreLibro = espacioDeMemoria();
 
     printf("Precio del libro: ");
     scanf("%f", lib->precioLibro);
 
-    printf("Año de publicacion: ");
-    scanf("%d", lib->anioPublicacion);
+    printf("Cantidad: ");
+    scanf("%d", lib->cantidad);
 
     lib->codigoLibro += 1;
     printf("Libro registrado exitosamente, el codigo del libro es el siguiente: %d\n", lib->codigoLibro);
+    int c;
+
+    while ((c= getchar()) != '\n' && c != EOF) {
+
+    }
 }
 
 void LiberarMemoria(Libro *lib) {
     free(lib->nombreLibro);
     free(lib->precioLibro);
-    free(lib->caracteres);
+    //free(lib->caracteres);
     //free(lib->codigoLibro);
-    free(lib->anioPublicacion);
+    free(lib->cantidad);
     lib->nombreLibro = NULL;
     lib->precioLibro = NULL;
-    lib->caracteres = NULL;
-    lib->anioPublicacion = NULL;
+    //lib->caracteres = NULL;
+    lib->cantidad = NULL;
 }
 
 int main() {
-
-
-    // Crear libro vacío
     
     Libro lib = CrearLibro();
-
-    // Registrar datos desde el usuario
     registrarLibro(&lib);
-
-    // Liberar memoria
     LiberarMemoria(&lib);
 
     return 0;
--- buffer.h
+++ buffer.h
@@ -5,24 +5,34 @@
 #define BUFFER_H
 
 // Lee la cadena caracter por caracter para determinar su espacio de memoria
-char* espacioDeMemoria(char* cadena) {
+char* espacioDeMemoria() {
     
-    //char* capacidad = NULL;
+    char* cadena = NULL;
     int caracteres = 0;
+    int c;
+    c = getchar();
 
-    while (cadena[caracteres] != '\n' && cadena[caracteres] != '\0') {
+    while (( c = getchar()) != '\n' && c != EOF) {
+        char *capacidad = realloc(cadena,( caracteres+2) * sizeof(char));//Se suma 2 para que quepa el caracter y '/0'
+        if (capacidad == NULL) {
+            printf("Error en la asignacion de memoria\n");
+            free(capacidad);
+            exit(1);
+        }
+        cadena = capacidad;
+        cadena[caracteres] = (char)c;//'\0'
         caracteres+=1;
     }
-    char* capacidad = (char*)malloc((caracteres+1) * sizeof(char));
-    if (capacidad == NULL) {
-        printf("Error en la asignacion de memoria\n");
-        //free(capacidad);
-        exit(1);
-    }
+    //char* capacidad = (char*)malloc((caracteres+1) * sizeof(char));
 
-    strncpy(capacidad, cadena, caracteres);
-    capacidad[caracteres] = '\0';
-    return capacidad;
+    //strncpy(capacidad, cadena, caracteres);
+    if (cadena != NULL) {
+        cadena[caracteres] = '\0';
+    }
+    //capacidad[caracteres] = '\0';
+
+    //return capacidad;
+    return cadena;
 }
 
 #endif
